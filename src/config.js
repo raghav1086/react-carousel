d58a6d8ca25738049f479f6db5c79d9a7f2344d4
Raghav <singh.raghverndra@pwc.com>
2019-02-23 22:49:15 +0800

    Updated config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
+//**
+// This file supposed to pick
+// values from process.env
+//*************************
 const Config = {
-  API_URL: "https://pixabay.com/api/",
-  API_KEY: "9656065-a4094594c34f9ac14c7fc4c39"
+  API_URL: process.env.REACT_APP_API_URL || "https://pixabay.com/api/",
+  API_KEY: process.env.REACT_APP_API_KEY || "9656065-a4094594c34f9ac14c7fc4c39"
 }
 
 export default Config
