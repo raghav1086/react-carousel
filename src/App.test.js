e13254fc42f0a351d5edc1d6b3b5da51d58a52e2
Raghav <singh.raghverndra@pwc.com>
2019-02-23 22:01:47 +0800

    added test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('Carousel', () => {
+  it('calls componentDidMount', () => {
+    sinon.spy(App.prototype, 'componentDidMount');
+    const wrapper = mount(<App />);
+    expect(wrapper.find("section")).to.have.lengthOf(1)
+    expect(App.prototype.componentDidMount).to.have.property('callCount', 1);
+    App.prototype.componentDidMount.restore();
+  });
 });
